b6212c8114070fc81f5adcc7b1f10fe82cce77bb
jesus <chuygs12@gmail.com>
2023-12-31 13:26:16 -0600

    controller for user

---
+++ server/controllers/user.js
@@ -0,0 +1,130 @@
+import User from "../models/user"
+
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const updateNameUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { name } = req.body
+    const user = await User.findByIdAndUpdate(id, { name })
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findByIdAndDelete(id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+
+export const getFriends = async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    )
+    return res.status(200).json(friends)
+
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const sendFriendRequest = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { friendId } = req.body
+
+    const user = await User.findById(id)
+    const friend = await User.findById(friendId)
+
+    user.requests.push(friendId)
+    friend.requests.push(id)
+
+    await user.save()
+    await friend.save()
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const acceptFriendRequest = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { friendId } = req.body
+
+    const user = await User.findById(id)
+    const friend = await User.findById(friendId)
+
+    if (user.friends.includes(friendId)) {
+      return res.status(400).json({ message: "Already friends" })
+    } else {
+      user.friends.push(friendId)
+      friend.friends.push(id)
+      await user.save()
+      await friend.save()
+      res.status(200).json(user)
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const declineFriendRequest = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { friendId } = req.body
+
+    const user = await User.findById(id)
+    const friend = await User.findById(friendId)
+
+    user.requests = user.requests.filter((id) => id !== friendId)
+    friend.requests = friend.requests.filter((id) => id !== id)
+
+    await user.save()
+    await friend.save()
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }  
+}
+
+export const deleteFriend = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { friendId } = req.body
+
+    const user = await User.findById(id)
+    const friend = await User.findById(friendId)
+
+    user.friends = user.friends.filter((id) => id !== friendId)
+    friend.friends = friend.friends.filter((id) => id !== id)
+
+    await user.save()
+    await friend.save()
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
